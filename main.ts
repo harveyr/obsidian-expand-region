a9260a7b0dcaa74ed7589b2cbcd274268db60cc5
Harvey Rogers <harveyr@gmail.com>
2023-03-11 09:20:37 -0800

    first expansion is working

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-import { Editor, MarkdownView, Plugin } from "obsidian";
+import {
+	Editor,
+	EditorSelection,
+	EditorPosition,
+	MarkdownView,
+	Plugin,
+} from "obsidian";
+import { cursorTo } from "readline";
 
 // Remember to rename these classes and interfaces!
 
@@ -14,11 +21,57 @@ export default class MyPlugin extends Plugin {
 		this.addCommand({
 			id: "expand-selection-region-command",
 			name: "Expand Region",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				// const selections = editor.listSelections();
-				// editor.replaceSelection("Sample Editor Command");
-			},
+			editorCallback: this.handleExpandCommand,
 		});
 	}
+
+	handleExpandCommand(editor: Editor, view: MarkdownView) {
+		const selections = editor.listSelections();
+
+		if (selections.length > 1) {
+			console.log("Not expanding multiple selections");
+			return;
+		}
+
+		const currSel: EditorSelection = selections[0];
+		console.log("current selection", currSel);
+
+		if (currSel.anchor.line != currSel.head.line) {
+			console.log("I don't handle multiple lines yet");
+			return;
+		}
+
+		const line = editor.getLine(currSel.anchor.line);
+
+		let newStart = currSel.anchor.ch;
+		const newEnd = currSel.head.ch;
+
+		while (newStart > 0) {
+			const startChar = line[newStart];
+			if (isDelimiter(startChar)) {
+				break;
+			}
+			newStart--;
+		}
+
+		// const line = editor.getCursor
+		// editor.replaceSelection("Sample Editor Command");
+
+		const newAnchor: EditorPosition = {
+			line: currSel.anchor.line,
+			ch: newStart,
+		};
+		const newHead: EditorPosition = {
+			line: currSel.anchor.line,
+			ch: newEnd,
+		};
+
+		editor.setSelection(newAnchor, newHead);
+	}
+}
+
+function isDelimiter(char: string): boolean {
+	const delims = ["[", "]", ")", ")", " ", "-"];
+
+	return delims.indexOf(char) >= 0;
 }
